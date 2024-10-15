f500e7e54d6a76b8882951443fcd0946689ed794
YahirMaster777 <yahirperez141002@gmail.com>
2024-10-15 00:35:15 -0600

    Agrega función para obtener empleados del view

--- crud/view.py
+++ crud/view.py
@@ -2,7 +2,7 @@ import models.view
 from sqlalchemy.orm import Session
 
 import models.view
-
+#obtener empleados del view
 def get_empleados(db: Session, skip: int = 0, limit: int = 30):
     return db.query(models.view.EmpleadoDireccion).offset(skip).limit(limit).all()
 
